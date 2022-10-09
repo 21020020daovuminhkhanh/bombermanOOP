e12d004b6e928d0d7a3e45d119a1fc8d7730c781
21020020daovuminhkhanh <100520334+21020020daovuminhkhanh@users.noreply.github.com>
2022-10-09 22:13:24 +0700

    Update main

--- src/bomberman/Main.java
+++ src/bomberman/Main.java
@@ -6,6 +6,10 @@ import java.awt.*;
 
 public class Main extends Canvas{
     public static void main(String[] args) {
-
+        JFrame frame = new JFrame();
+        frame.setSize(20 * 16 * 3, 13 * 16 * 3);
+        frame.setResizable(false);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
     }
 }
